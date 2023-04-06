2997966a2c6abbcef4c83837ff59e08477fe9f47
Chris Pitts <chris@thirstybear.co.uk>
2023-04-06 14:51:33 +0100

    Convert C# files to Java
    Line by line conversion, converting the coding language directly to Java with minimal change.

---
+++ java/src/test/java/org/example/ExampleCharacterTestCase.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleCharacterTestCase {
+  @Test
+  public void findCharacterByLasName() {
+    // This test constructs all its own test data to make it clearer what it does
+    var karen = new Character("Karen", "Wheeler");
+    var mike = new Character("Mike", "Wheeler");
+
+    // this is example code showing kinds of assertion you could do on a Character
+    var nancy = new Character("Nancy", "Wheeler");
+    assertEquals("Nancy", nancy.firstName);
+    assertEquals(nancy, nancy);
+    // This assertion works because Character is a ValueObject that implements "Equals"
+    assertEquals(new Character("Nancy", "Wheeler"), nancy);
+
+    karen.AddChild(nancy);
+    karen.AddChild(mike);
+    var finder = new CharacterFinder(Arrays.asList(
+        karen,
+        mike,
+        nancy));
+
+    // This is the 'act' step
+    var charactersList = finder.findFamilyByLastName("Wheeler");
+
+    // this is example code showing kinds of assertion you could do on a List of Characters
+    assertNotNull(charactersList);
+    assertEquals(3, charactersList.size());
+    assertTrue(charactersList.contains(new Character("Nancy", "Wheeler")));
+    assertTrue(charactersList.containsAll(Arrays.asList(
+        new Character("Nancy", "Wheeler"),
+        new Character("Mike", "Wheeler"),
+        new Character("Karen", "Wheeler"))));
+    assertEquals(Arrays.asList(
+        new Character("Karen", "Wheeler"),
+        new Character("Mike", "Wheeler"),
+        new Character("Nancy", "Wheeler")), charactersList);
+  }
+}
